ee2fa8f991b3614f42adbc15f80e449851e4218a
JokerHAHAHA <liondejude@hotmail.com>
2016-03-31 22:50:50 +0200

    add unicorn

--- public/js/controllers/userController.js
+++ public/js/controllers/userController.js
@@ -15,12 +15,12 @@ function userController($scope, $location, $http, hikeService, userService, user
 	}
 
 	$scope.add = function() {
-		
+
 		$scope.passwordOk = 0;
 		$scope.mailOk = 0;
 
 		userService.create($scope.data).then(function(res){
-			
+
 			if (res.data.message) {
 				if (res.data.message == "Wrong password") {
 					$scope.passwordOk = 1;
@@ -28,7 +28,7 @@ function userController($scope, $location, $http, hikeService, userService, user
 				} else {
 					$scope.mailOk = 1;
 					$scope.data.password = "";
-					
+
 				}
 			} else {
 				userFactory.currentUser = res.data;
@@ -37,8 +37,8 @@ function userController($scope, $location, $http, hikeService, userService, user
 				$scope.viewCount = 2;
 			}
 		});
-		
-		
+
+
 
 
 	}
