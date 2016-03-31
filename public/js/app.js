f9c26070db1ac9440ed3b16911284a9f1b70fd24
Jerome Bar <barjerome@hotmail.com>
2016-03-31 18:47:34 +0200

    add function route on the map

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,14 @@
 angular.module('app', ['ngRoute', 'ngAnimate', 'uiGmapgoogle-maps'])
-    .config(routes)
-    .controller('mainController', mainController)
-    .controller('hikingController', hikingController)
-    .controller('bikingController', bikingController)
-    .controller('mapController', mapController)
-    .controller('userController', userController)
-    .service('hikeService', hikeService)
-    .service('mapService', mapService);
+	.config(routes)
+	.controller('mainController', mainController)
+	.controller('hikingController', hikingController)
+	.controller('bikingController', bikingController)
+	.controller('mapController', mapController)
+	.controller('userController', userController)
+	.service('hikeService', hikeService)
+	.service('mapService', mapService)
+	.config(function(uiGmapGoogleMapApiProvider) {
+		uiGmapGoogleMapApiProvider.configure({
+			libraries: 'weather,geometry,visualization'
+		});
+	});
--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -1,3 +1,32 @@
-function mapController($scope, $location, $http, hikeService, mapService) {
-	
+function mapController($scope, $location, $http, hikeService, mapService,uiGmapGoogleMapApi) {
+	$scope.map = { center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4, bounds: {}};
+	$scope.polylines = [];
+	uiGmapGoogleMapApi.then(function(){
+		$scope.polylines = [
+			{	
+				path: [
+					{
+						latitude: 45,
+						longitude: -74
+					},
+					{
+						latitude: 30,
+						longitude: -89
+					},
+					{
+						latitude: 37,
+						longitude: -122
+					},
+					{
+						latitude: 60,
+						longitude: -95
+					}
+				]
+			}
+		]});
 }
+
+
+
+
+
